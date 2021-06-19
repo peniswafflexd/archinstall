5feabc607d0003db79edb9cefed6c4a85c0fb9ec
Devon Gregory <gregordevo@ecs.vuw.ac.nz>
2021-06-19 22:47:13 +1200

    change su nobody to sudo -u nobody for makepkg

--- installer.sh
+++ installer.sh
@@ -6,26 +6,30 @@
 
 systemctl start dhcpcd && systemctl enable dhcpcd
 
-sudo pacman -Syu --noconfirm base-devel xorg xorg-xinit xmonad xmonad-contrib xmobar alacritty nitrogen dmenu virtualbox-guest-utils firefox picom
+sudo pacman -Syu --noconfirm base-devel xorg xorg-xinit xmonad xmonad-contrib xmobar alacritty nitrogen dmenu virtualbox-guest-utils firefox picom checkupdates
 
 git clone https://aur.archlinux.org/yay-git.git
 
-cd yay-git && su nobody -c "makepkg --noconfirm -si"
+cd yay-git && sudo -u nobody makepkg --noconfirm -si
 
 cd ../
 
-su nobody -c "yay -S --noconfirm nerd-fonts-mononoki otf-font-awesome-5-free sublime-text"
+sudo -u nobody yay -S --noconfirm nerd-fonts-mononoki otf-font-awesome-5-free sublime-text
 
 cp -r .xmonad ~
 cp -r .config ~
 cp -r .xinitrc ~
 cp -r wallpapers ~
 
-if [ ! -d "~/.local/bin/" ] 
+if [ ! -d "/home/peniswaffle/.local" ] 
 then
-    mkdir ~/.local/bin/
+    mkdir ~/.local
 fi
-echo "cupd=$(checkupdates | wc -l) \n echo \"$cupd updates\"" >> ~/.local/bin/pacupdate
+if [ ! -d "/home/peniswaffle/.local/bin/" ] 
+then
+    mkdir ~/.local/bin
+fi
+echo "cupd=$(checkupdates | wc -l) \n echo \"$cupd updates\"" > ~/.local/bin/pacupdate
 
 
 nitrogen --set-scaled --random ~/wallpapers
